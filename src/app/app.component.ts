218222cc114e07178910338bbec4496eea09ffa4
luca.alicata <luca.alicata@lynxspa.com>
2020-03-13 15:45:54 +0100

    utilizzo base si interpolazione e property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DemoMilano';
+
+  html= '<div>Ciao</div>';
+  
+  link = 'https://google.it';
+
+  constructor(){
+    
+  }
 }
