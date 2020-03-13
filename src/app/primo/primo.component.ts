7b8997d3a92c7345551b2c2226f8de4b1b9731e3
luca.alicata <luca.alicata@lynxspa.com>
2020-03-13 15:56:26 +0100

    creazione componente figlio e utilizzo property bingind per un input

---
+++ src/app/primo/primo.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-primo',
+  templateUrl: './primo.component.html',
+  styleUrls: ['./primo.component.scss']
+})
+export class PrimoComponent implements OnInit {
+  @Input()
+  parin: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
