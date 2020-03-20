6dfa1be841f9c1b336efcaa652e00468eb2a5b04
luca.alicata <luca.alicata@lynxspa.com>
2020-03-20 09:50:21 +0100

    fine configurazione routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router;
+import { RouterModule, Routes } from '@angular/router';
 import { PrimoComponent } from './primo/primo.component';
 import { SecondoComponent } from './secondo/secondo.component';
 import { CicloVitaComponent } from './ciclo-vita/ciclo-vita.component';
 
 const appRoutes: Routes = [
-    { path: 'primo', component: PrimoComponent },
+    { path: 'primo', component: PrimoComponent, data: {title: 'titolo'} },
+    { path: 'primo-diverso', component: PrimoComponent, data: { title: 'Diverso' } },
     { path: 'secondo', component: SecondoComponent },
-    { path: 'ciclo-vita', component: CicloVitaComponent }
+    { path: 'ciclo-vita', component: CicloVitaComponent },
+    { path: '', redirectTo: '/primo', pathMatch: 'full' },
 ];
 
 @NgModule({
