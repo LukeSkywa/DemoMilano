c614178e8dd9d9339aafa6d1ddbb5ebd6942355b
luca.alicata <luca.alicata@lynxspa.com>
2020-03-25 09:38:58 +0100

    giochi con i campi di input e il loro alias

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -5,13 +5,14 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class HighlightDirective {
 
-  @Input() highlightColor: string;
+  @Input('appHighlight') highlightColor: string;
 
   constructor(private el: ElementRef) { 
   }
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.el.nativeElement.style.backgroundColor = this.highlightColor;
+    this.el.nativeElement.style.backgroundColor = this.highlightColor != null && this.highlightColor !== ''
+    ? this.highlightColor : 'red';
   }
 
   @HostListener('mouseleave') onMouseLeave() {
