41d2899a61bdd41445f0a90669bda725ea1fc2f7
luca.alicata <luca.alicata@lynxspa.com>
2020-04-01 17:27:41 +0200

    gestione errori nelle chiamate http

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MyHttpService } from '../my-http.service';
 import { GameItem } from 'src/game-item.interface';
+import { HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-games',
@@ -13,9 +15,30 @@ export class GamesComponent implements OnInit {
   constructor(private myHttpService: MyHttpService) { }
 
   ngOnInit(): void {
-    this.myHttpService.getGames().subscribe(value=>{
-      this.gameList = value;
+    this.myHttpService.getGames().subscribe(value => {
+      console.log(JSON.stringify(value));
+      this.gameList = value.body;
+    }, err => {
+      console.log('Servizio andato in errore');
     });
+
+    // possibile gestione di errore della chiamata
+    this.myHttpService.getCallInError().subscribe(value => {
+    }, (error: HttpErrorResponse) => {
+      console.log('Servizio andato in errore');
+    });
+
+    // possibile gestione di errore della chiamata
+    this.myHttpService.getCallInError().pipe(
+      catchError(error => {
+        return ['ho fatto il catch dell\'errore']
+      })
+    ).subscribe(value => {
+      console.log(value);
+    }, (error: HttpErrorResponse) => {
+      console.log('Servizio andato in errore');
+    });
+
   }
 
 }
--- src/app/my-http.service.ts
+++ src/app/my-http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { GameItem } from 'src/game-item.interface';
 
@@ -10,7 +10,12 @@ export class MyHttpService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getGames(): Observable<GameItem[]>{
-    return this.httpClient.get<GameItem[]>('http://localhost:3000/games');
+  getGames(): Observable<HttpResponse<GameItem[]>>{
+    return this.httpClient.get<GameItem[]>('http://localhost:3000/games', 
+    { observe: 'response' });
+  }
+
+  getCallInError(){
+    return this.httpClient.get('http://localhost:3000/gams');
   }
 }
