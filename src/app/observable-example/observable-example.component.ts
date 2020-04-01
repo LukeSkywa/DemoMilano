7f724a4de2d4491705e325264573d07013fba09d
luca.alicata <luca.alicata@lynxspa.com>
2020-04-01 15:11:24 +0200

    altri esempi con anche l'interval e timer

--- src/app/observable-example/observable-example.component.ts
+++ src/app/observable-example/observable-example.component.ts
@@ -1,26 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of, from } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, of, from, interval, timer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-observable-example',
   templateUrl: './observable-example.component.html',
   styleUrls: ['./observable-example.component.scss']
 })
-export class ObservableExampleComponent implements OnInit {
+export class ObservableExampleComponent implements OnInit, OnDestroy {
+
+  private intervalSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit(): void {
-    const obs1$ = of(1,2,3);
-    obs1$.subscribe(value=>{
-      console.log("Valore1 of: "+value);
+    const obs1$ = of(1, 2, 3);
+    obs1$.subscribe(value => {
+      console.log("Valore1 of: " + value);
     });
-    obs1$.subscribe(value=>{
-      console.log("Valore2 of: "+value);
+    obs1$.subscribe(value => {
+      console.log("Valore2 of: " + value);
     });
-    from([1,2,3]).subscribe(value=>{
-      console.log("Valore from: "+value);
+    from([1, 2, 3]).subscribe(value => {
+      console.log("Valore from: " + value);
     });
+
+    this.intervalSubscription = interval(1000).subscribe({
+      next: n => {
+        console.log(`It's been ${n} seconds since subscribing!`)
+      },
+      error: null,
+      complete: ()=> {console.log('interval complete')}
+    });
+    
+    timer(5000).subscribe({
+      next: n => {
+        console.log(`Timer`)
+      },
+      complete: ()=> {console.log('timer complete')}
+    });
+
+  }
+
+  ngOnDestroy(): void {
+    this.intervalSubscription.unsubscribe();
   }
 
 }
